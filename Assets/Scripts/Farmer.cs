8702199e456b44ede3139305726aa1b4ce5bade6
zubcarz <zubcarz@gmail.com>
2018-07-11 18:40:25 -0500

    Farmer controller

---
+++ Assets/Scripts/Farmer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+public class Farmer : MonoBehaviour {
+
+    Animator playerAnimator;
+    Rigidbody rb;
+
+    public float forwardSpeed = 1f;
+    public float turnSpeed = 1f;
+
+    // Use this for initialization
+    void Awake()
+    {
+        playerAnimator = GetComponent<Animator>();
+        playerAnimator.applyRootMotion = true;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerAnimator.applyRootMotion = Grounded();
+        playerAnimator.SetBool("grounded", Grounded());
+    }
+
+    public bool Grounded()
+    {
+        return Mathf.Abs(Vector3.Dot(rb.velocity, Vector3.up)) < 0.01;
+    }
+        
+
+    public void SetTurnSpeed(float speed)
+    {
+        playerAnimator.SetFloat("turnSpeed", speed);
+        var rot = transform.rotation;
+        rot.y += Time.deltaTime * turnSpeed * speed;
+        transform.rotation = rot;
+    }
+
+    public void SetForwardSpeed(float speed)
+    {
+        playerAnimator.SetFloat("forwardSpeed", speed);
+
+        rb.velocity += transform.forward * forwardSpeed * ((speed > 0)? speed : 0);
+    }
+}
---
+++ Assets/Scripts/FarmerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Farmer))]
+public class FarmerControl : MonoBehaviour {
+
+    Farmer farmer;
+
+    // Use this for initialization
+    void Start()
+    {
+        farmer = GetComponent<Farmer>();
+    }
+
+    void FixedUpdate()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        farmer.SetForwardSpeed(verticalInput);
+        farmer.SetTurnSpeed(horizontalInput);
+    }
+}
